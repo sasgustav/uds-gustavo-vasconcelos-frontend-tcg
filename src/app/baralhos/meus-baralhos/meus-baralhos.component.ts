7aa7aeef392580cc89cb8795c6ad66c508f4667f
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2024-05-09 00:47:58 -0300

    asd

--- src/app/baralhos/meus-baralhos/meus-baralhos.component.ts
+++ src/app/baralhos/meus-baralhos/meus-baralhos.component.ts
@@ -10,13 +10,21 @@ export class MeusBaralhosComponent implements OnInit {
 
   ngOnInit() {
     this.carregarBaralhos();
-    console.log(this.baralhos);
-  }  
+  }
 
   carregarBaralhos() {
-    this.baralhos = JSON.parse(localStorage.getItem('baralhoCriadoPeloUsuario') || '[]');
-    console.log(this.baralhos);
-  }  
+    const dados = localStorage.getItem('baralhoCriadoPeloUsuario');
+    if (dados) {
+      const objetosBaralhos = JSON.parse(dados);
+      this.baralhos = Object.keys(objetosBaralhos).map(key => ({
+        nome: key,
+        ...objetosBaralhos[key]
+      }));
+      console.log('Baralhos carregados:', this.baralhos);
+    } else {
+      console.log('Nenhum dado encontrado no localStorage.');
+    }
+  }
 
   sumHP(cards: any[]): number {
     return cards.reduce((total, card) => total + Number(card.hp), 0);
@@ -29,7 +37,10 @@ export class MeusBaralhosComponent implements OnInit {
   removerBaralho(nomeDoBaralho: string) {
     if(confirm(`Tem certeza que deseja remover o baralho '${nomeDoBaralho}'?`)) {
       this.baralhos = this.baralhos.filter(baralho => baralho.nome !== nomeDoBaralho);
-      localStorage.setItem('baralhoCriadoPeloUsuario', JSON.stringify(this.baralhos));
+      localStorage.setItem('baralhoCriadoPeloUsuario', JSON.stringify(this.baralhos.reduce((acc, curr) => {
+        acc[curr.nome] = { cards: curr.cards };
+        return acc;
+      }, {})));
       alert('Baralho removido com sucesso.');
     }
   }
