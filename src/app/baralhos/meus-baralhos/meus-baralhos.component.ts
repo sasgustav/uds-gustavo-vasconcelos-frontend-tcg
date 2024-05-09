38531853fca15e1c6d074ac4ce70d3edb460059d
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2024-05-09 00:51:57 -0300

    hjk

--- src/app/baralhos/meus-baralhos/meus-baralhos.component.ts
+++ src/app/baralhos/meus-baralhos/meus-baralhos.component.ts
@@ -20,7 +20,6 @@ export class MeusBaralhosComponent implements OnInit {
         nome: key,
         ...objetosBaralhos[key]
       }));
-      console.log('Baralhos carregados:', this.baralhos);
     } else {
       console.log('Nenhum dado encontrado no localStorage.');
     }
@@ -35,7 +34,7 @@ export class MeusBaralhosComponent implements OnInit {
   }
 
   removerBaralho(nomeDoBaralho: string) {
-    if(confirm(`Tem certeza que deseja remover o baralho '${nomeDoBaralho}'?`)) {
+    if (confirm(`Tem certeza que deseja remover o baralho '${nomeDoBaralho}'?`)) {
       this.baralhos = this.baralhos.filter(baralho => baralho.nome !== nomeDoBaralho);
       localStorage.setItem('baralhoCriadoPeloUsuario', JSON.stringify(this.baralhos.reduce((acc, curr) => {
         acc[curr.nome] = { cards: curr.cards };
