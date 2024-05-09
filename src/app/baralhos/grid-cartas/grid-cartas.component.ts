613cc10e0e12dc3662cf18325764643fe90bf726
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2024-05-08 23:32:23 -0300

    asdasd

--- src/app/baralhos/grid-cartas/grid-cartas.component.ts
+++ src/app/baralhos/grid-cartas/grid-cartas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { finalize } from 'rxjs';
 import { Carta } from 'src/app/models/carta.model';
 import { PokemonTcgService } from 'src/app/services/pokemon-tcg.service';
 
@@ -9,17 +9,26 @@ import { PokemonTcgService } from 'src/app/services/pokemon-tcg.service';
   styleUrls: ['./grid-cartas.component.scss']
 })
 export class GridCartasComponent implements OnInit {
-  cartas!: Observable<Carta[]>;
-  isLoading!: Observable<boolean>;
+  cartas: Carta[] = [];
+  isLoading: boolean = false; 
   cartaSelecionada?: Carta;
   modalVisivel: boolean = false;
 
   constructor(private pokemonTcgService: PokemonTcgService) {}
 
   ngOnInit() {
-    this.cartas = this.pokemonTcgService.getCartas();
+    this.carregarCartas();
   }
 
+  carregarCartas() {
+    this.isLoading = true;  // Inicia o carregamento
+    this.pokemonTcgService.getCartas()
+      .pipe(finalize(() => this.isLoading = false))  // Finaliza o carregamento após a conclusão
+      .subscribe(cartas => {
+        this.cartas = cartas;  // Armazena as cartas recebidas
+      });
+  }
+  
   abrirModalDetalhes(carta: Carta) {
     this.cartaSelecionada = carta;
     this.modalVisivel = true;
@@ -27,7 +36,6 @@ export class GridCartasComponent implements OnInit {
   
   
   fecharModal() {
-    console.log("Closing modal");
     this.modalVisivel = false;
     this.cartaSelecionada = undefined;
   }  
