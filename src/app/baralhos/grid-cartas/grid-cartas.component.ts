b9c77c71c78cc0913167018764e8c4bfc5acb022
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2024-05-08 23:32:56 -0300

    dfgh

--- src/app/baralhos/grid-cartas/grid-cartas.component.ts
+++ src/app/baralhos/grid-cartas/grid-cartas.component.ts
@@ -10,33 +10,32 @@ import { PokemonTcgService } from 'src/app/services/pokemon-tcg.service';
 })
 export class GridCartasComponent implements OnInit {
   cartas: Carta[] = [];
-  isLoading: boolean = false; 
+  isLoading: boolean = false;
   cartaSelecionada?: Carta;
   modalVisivel: boolean = false;
 
-  constructor(private pokemonTcgService: PokemonTcgService) {}
+  constructor(private pokemonTcgService: PokemonTcgService) { }
 
   ngOnInit() {
     this.carregarCartas();
   }
 
   carregarCartas() {
-    this.isLoading = true;  // Inicia o carregamento
+    this.isLoading = true;
     this.pokemonTcgService.getCartas()
-      .pipe(finalize(() => this.isLoading = false))  // Finaliza o carregamento após a conclusão
+      .pipe(finalize(() => this.isLoading = false))
       .subscribe(cartas => {
-        this.cartas = cartas;  // Armazena as cartas recebidas
+        this.cartas = cartas;
       });
   }
-  
+
   abrirModalDetalhes(carta: Carta) {
     this.cartaSelecionada = carta;
     this.modalVisivel = true;
   }
-  
-  
+
   fecharModal() {
     this.modalVisivel = false;
     this.cartaSelecionada = undefined;
-  }  
+  }
 }
